e85c908afa396678916985296cc85d7e3a3f5833
harshidwani <kapeeddeepak61@gmail.com>
2025-07-01 00:36:18 +0530

    transfering code before changing pinecone model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="generative-ai-template",
+    version="0.0.0",
+    author="harsh idwani",
+    author_email="kapeeddeepak61@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
